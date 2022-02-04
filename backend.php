a60cfe1edbd9d444d0fe66ca385e578ad7529d20
Tris10 <tristan.aeschbach@gmail.com>
2022-02-04 21:42:56 +0100

    pp

--- backend.php
+++ backend.php
@@ -1,48 +1,36 @@
 <?php
-session_start();
+if(!isset($_SESSION)){
+    session_start();
+}
 if(isset($_POST['usernameLogin']) && isset($_POST['passwordLogin'])){
-    $host     = 'localhost';       // host
-    $password = '123456';        // Passwort (brauchen Sie nie dieses Passwort)
-    $database = 'm151';   // database
-    $username = 'userM151';
+    $mysqli = dbconnector(1);
 
-// Verbindung herstellen
-    $mysqli = new mysqli($host, $username, $password, $database);
-
-// Verbindung prüfen
-    if ($mysqli->connect_error) {
-        die("Verbindung misslungen: " . $mysqli->connect_error);
-    }
-    echo "test0";
     $username = htmlspecialchars(trim($_POST['usernameLogin']));
     $password = htmlspecialchars($_POST['passwordLogin']);
     $result = $mysqli->query("SELECT * from users where username = '$username'");
     if ($result->num_rows == 1) {
-        echo "test1";
         while($row = $result->fetch_assoc()) {
-            echo "test2";
             if(password_verify($password, $row['hash']) && !empty($row['username'])){
-                echo "test3";
                 $_SESSION['ID'] = $row['ID'];
                 $_SESSION['username'] = $row['username'];
                 $_SESSION['firstName'] = $row['firstName'];
                 $_SESSION['lastName'] = $row['lastName'];
                 $_SESSION['admin'] = $row['status'];
                 session_regenerate_id();
-                print_r($_SESSION);
+                if($row['status'] == 0){
+                    $_SESSION['page'] = "user";
+                }else{
+                    $_SESSION['page'] = "admin";
+                }
             }else{
                 echo "Benutzername oder Passwort sind falsch";
-                sleep(10);
             }
+            $mysqli->close();
             echo "<meta http-equiv='refresh' content='0;url=index.php'>";
         }
     }
+}
 
-}
-if(isset($_GET['logout'])){
-    session_destroy();
-    echo "<meta http-equiv='refresh' content='0;url=index.php'>";
-}
 function dbconnector($admin){
     $host     = 'localhost';       // host
     $password = '123456';        // Passwort (brauchen Sie nie dieses Passwort)
@@ -54,11 +42,215 @@ function dbconnector($admin){
         $username = 'userM151';
     }
 // Verbindung herstellen
-    $conn = mysqli_connect($host, $username, $password, $database);
+    $mysqli = new mysqli($host, $username, $password, $database);
 
 // Verbindung prüfen
-    if ($conn->connect_error) {
-        die("Verbindung misslungen: " . $conn->connect_error);
+    if ($mysqli->connect_error) {
+        die("Verbindung misslungen: " . $mysqli->connect_error);
     }
-    return $conn;
+    return $mysqli;
+}
+function userPage(){
+    return "you are user";
+}
+
+function adminPage(){
+    $output = '<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">ID</th>
+      <th scope="col">Username</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Categories</th>
+      <th scope="col">Admin</th>
+    </tr>
+  </thead>
+  <tbody>';
+    $mysqli = dbconnector(1);
+    $i = 0;
+    $result = $mysqli->query("SELECT * from users;");
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $id = $row['ID'];
+            $username = $row['username'];
+            $firstName = $row['firstName'];
+            $lastName = $row['lastName'];
+            $status = $row['status'];
+
+            $i++;
+            $output .= "<tr>
+                          <th scope='row'>#$i</th>
+                          <td>$id</td>
+                          <td>$username</td>
+                          <td>$firstName</td>
+                          <td>$lastName</td>
+                          <td>WIP</td>
+                          <td>$status</td>
+                        </tr>";
+        }
+    }else{
+        $output .= "no results";
+    }
+    $output .= "</tbody></table>";
+    $mysqli->close();
+    return $output;
+}
+function login(){
+    return '<div class="container">
+                <div class="row vertical-offset-100">
+                    <div class="col-md-4 col-md-offset-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Please sign in</h3>
+                            </div>
+                            <div class="panel-body">
+                                <form accept-charset="UTF-8" role="form" method="post" action="backend.php">
+                                <fieldset>
+                                    <div class="form-group">
+                                        <input class="form-control" placeholder="Username" name="usernameLogin" type="text" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control" placeholder="Password" name="passwordLogin" type="password" value="" required>
+                                    </div>
+                                    <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+                                </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>';
+}
+
+if(isset($_GET['logout'])){
+    logout();
+}
+function logout(){
+    session_destroy();
+    echo "<meta http-equiv='refresh' content='0;url=index.php'>";
+}
+
+if(isset($_GET['page']) && $_GET['page'] == "default"){
+    if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1){
+        $_SESSION['page'] = "admin";
+    }elseif (isset($_SESSION['admin']) && $_SESSION['admin'] == 0){
+        $_SESSION['page'] = "user";
+    }else{
+        $_SESSION['page'] = "login";
+    }
+    echo "<meta http-equiv='refresh' content='0;url=index.php'>";
+}
+if(isset($_GET['page']) && $_GET['page'] == "newUser"){
+    $_SESSION['page'] = "newUser";
+    echo "<meta http-equiv='refresh' content='0;url=index.php'>";
+}
+function userForm($username = "", $firstName = "", $lastName = "", $categories = [""], $status = ""){
+    $output = "<div class='container'>
+    <h1>Registrierung</h1>
+
+    <form action='backend.php' method='post'>
+        <!-- benutzername -->
+        <div class='form-group'>
+            <label for='username'>Username *</label>
+            <input type='text' name='username' class='form-control' id='username'
+            value='$username'
+                   placeholder='Username'
+                   maxlength='30' required>
+        </div>
+        <!-- vorname -->
+        <div class='form-group'>
+            <label for='firstname'>First Name *</label>
+            <input type='text' name='firstname' class='form-control' id='firstname'
+            value='$firstName'
+                   placeholder='First Name'
+                   required>
+        </div>
+        <!-- nachname -->
+        <div class='form-group'>
+            <label for='lastname'>Last Name *</label>
+            <input type='text' name='lastname' class='form-control' id='lastname'
+            value='$lastName'
+                   placeholder='Last Name'
+                   maxlength='30'
+                   required>
+        </div>
+        <!-- password -->
+        <div class='form-group'>
+            <label for='password'>Password *</label>
+            <input type='password' name='password' class='form-control' id='password'
+                   placeholder='Password'
+                   required>
+        </div>
+        <!-- admin -->
+        <div class='form-group form-check'>
+            <input type='checkbox' class='form-check-input' $status name='status' id='status'>
+            <label class='form-check-label' for='status' >Admin</label>
+        </div>
+        <!-- categories -->
+        <div class='form-group form-check'>";
+    $mysqli = dbconnector(1);
+    $result = $mysqli->query("SELECT * from category;");
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $category = $row['name'];
+            $categoryID = $row['tag_ID'];
+            $output .= "<input type='checkbox' class='form-check-input' name='check$categoryID' id='check$categoryID'>
+            <label class='ml-1 form-check-label' for='check$categoryID'>$category</label>";
+        }
+    }
+    $output .= '</div>
+            <button type="submit" name="button" value="submit" class="btn btn-info" id="submitUser">Submit</button>
+          </form>
+        </div>';
+    $mysqli->close();
+    return $output;
+}
+if(isset($_POST['username'])){
+    $categories = array();
+    $mysqli = dbconnector(1);
+    $result = $mysqli->query("SELECT tag_ID from category;");
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $ID = $row['tag_ID'];
+            if(isset($_POST["check$ID"])){
+                $categories[$ID] = $ID;
+            }
+        }
+    }
+    if(isset($_POST["status"])){
+        $status = 1;
+    }else{
+        $status = 0;
+    }
+    createUser($_POST['username'], $_POST['firstname'], $_POST['lastname'], $_POST['password'], $status, $categories);
+    $mysqli->close();
+}
+function createUser($username, $firstName, $lastName, $password, $status, $categories){
+    $mysqli = dbconnector(1);
+    $username = trim(htmlspecialchars($username));
+    $firstName = trim(htmlspecialchars($firstName));
+    $lastName = trim(htmlspecialchars($lastName));
+    $password = password_hash(htmlspecialchars($password), PASSWORD_DEFAULT);
+    $stmt = $mysqli->prepare("INSERT INTO users (username, firstName, lastName, hash, Status) values (?,?,?,?,?);");
+    $stmt->bind_param("ssssi", $username, $firstName, $lastName, $password, $status);
+    $stmt->execute();
+    $stmt->close();
+    $mysqli->close();
+    asignCategories($username, $categories);
+}
+function asignCategories($username, $categories){
+    $mysqli = dbconnector(1);
+    $result = $mysqli->query("SELECT ID from users where username = $username;");
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $userID = $row['ID'];
+            foreach ($categories as $category){
+                $stmt = $mysqli->prepare("INSERT INTO users_has_kategorie (users_ID, kategorie_Tag_ID) values (?,?);");
+                $stmt->bind_param("ii", $userID, $category);
+                $stmt->execute();
+            }
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+include 'backend.php';
+if(!isset($_SESSION['page'])){
+    $_SESSION['page'] = "login";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +12,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>TO-DO</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
@@ -31,59 +35,76 @@ session_start();
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="backend.php?page=reee">TODO</a>
+                <a class="navbar-brand" href="backend.php?page=default">TODO</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="backend.php?page=raaa">New User</a></li>
-                    <li><a href="#">Link</a></li>
+                <?php
+                if(!isset($_SESSION['admin'])){
+                    echo '<ul class="nav navbar-nav">
+                    <li><p class="navbar-text">Please Login to continue</p></li>
+                </ul>';
+                }
+                if(isset($_SESSION['admin']) && $_SESSION['admin'] == 0){
+                    echo '<ul class="nav navbar-nav">
+                    <li><a href="backend.php?page=newUser">New TODO</a></li>
                 </ul>
                 <form class="navbar-form navbar-left">
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Search">
                     </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
                 </form>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="backend.php?logout=true"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout</a></li>
+                </ul>';
+                }
+                if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1){
+                    echo '<ul class="nav navbar-nav">
+                    <li><a href="backend.php?page=newUser">New User</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">New Category</a></li>
+                            <li><a href="#">Edit Category</a></li>
+                            <li><a href="#">Delete Category</a></li>
+                        </ul>
+                    </li>
                 </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="backend.php?logout=true"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Logout</a></li>
+                </ul>';
+                }
+                ?>
+
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
     <?php
-    if(!isset($_SESSION['username'])){
-        echo '<div class="container">
-                <div class="row vertical-offset-100">
-                    <div class="col-md-4 col-md-offset-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">Please sign in</h3>
-                            </div>
-                            <div class="panel-body">
-                                <form accept-charset="UTF-8" role="form" method="post" action="backend.php">
-                                <fieldset>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Username" name="usernameLogin" type="text" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Password" name="passwordLogin" type="password" value="" required>
-                                    </div>
-                                    <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
-                                </fieldset>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>';
+    if(isset($_SESSION['page']) && $_SESSION['page'] == "login"){
+        echo login();
+    }
+    if(isset($_SESSION['page']) && $_SESSION['page'] == "user"){
+        echo userPage();
+    }
+    if(isset($_SESSION['page']) && $_SESSION['page'] == "admin"){
+        echo adminPage();
+    }
+    if(isset($_SESSION['page']) && $_SESSION['page'] == "newUser"){
+        echo userForm();
     }
     ?>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
 
